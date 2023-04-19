db2e1937d55538fdff0e5acf3a209d5b43d6712c
UK014 <128369299+UK014@users.noreply.github.com>
2023-04-19 11:08:02 +0300

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,19 @@
+﻿function contact() {
+    if (document.getElementById("name").value == "") {
+        alert("The name field cannot be left blank.");
+    }
+    else if (document.getElementById("surname").value == "") {
+        alert("The surname field cannot be left blank.");
+    }
+    else if (document.getElementById("email1").value == "") {
+        alert("The e-mail field cannot be left blank.");
+    }
+    else if (document.getElementById("subject").value == "") {
+        alert("The subject field cannot be left blank.");
+    }
+    else if (document.getElementById("message").value == "") {
+        alert("The message field cannot be left blank.");
+    }
+
+
+}
---
+++ Resume.js
@@ -0,0 +1,44 @@
+﻿function start() {
+    window.open("/MIU/Resume");
+}
+document.getElementById("skillsbtn").addEventListener("click", addskills);
+function addskills() {
+    var skills = document.createElement("li");
+    skills.innerHTML = document.getElementById("Skills").value;
+    document.getElementById("skillslist").appendChild(skills);
+}
+document.getElementById("languagebtn").addEventListener("click", addlanguages);
+function addlanguages() {
+    var languages = document.createElement("li");
+    languages.innerHTML = document.getElementById("Language").value;
+    document.getElementById("languagelist").appendChild(languages);
+}
+function Degree() {
+    if (document.getElementById("Degree").value == "Bachelor") {
+        document.getElementById("masterdiv").style.visibility = "hidden";
+        document.getElementById("PhDdiv").style.visibility = "hidden";
+        document.getElementById("phdhr").style.visibility = "hidden";
+    }
+    if (document.getElementById("Degree").value == "Master") {
+        document.getElementById("masterdiv").style.visibility = "visible";
+        document.getElementById("PhDdiv").style.visibility = "hidden";
+        document.getElementById("phdhr").style.visibility = "hidden";
+    }
+    if (document.getElementById("Degree").value == "Ph.D") {
+        document.getElementById("masterdiv").style.visibility = "visible";
+        document.getElementById("PhDdiv").style.visibility = "visible";
+        document.getElementById("phdhr").style.visibility = "visible";
+    }
+
+}
+function experience() {
+    if (document.getElementById("workexperience").value == "jobone") {
+        document.getElementById("jobexperience2").style.visibility = "hidden";
+        document.getElementById("")
+    }
+    else if (document.getElementById("workexperience").value == "jobtwo") {
+        document.getElementById("jobexperience2").style.visibility = "visible";
+    }
+}
+
+
